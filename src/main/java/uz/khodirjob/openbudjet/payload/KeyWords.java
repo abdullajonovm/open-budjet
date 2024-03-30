d618e3e79f25ef03c34bbde68a4e3000c6dcce62
khodirjob <khodirjob@gmail.com>
2024-03-30 09:23:34 +0500

    commit

--- src/main/java/uz/khodirjob/openbudjet/payload/KeyWords.java
+++ src/main/java/uz/khodirjob/openbudjet/payload/KeyWords.java
@@ -7,7 +7,7 @@ public class KeyWords {
             SELECT_MESSAGE = "Iltimos tanlang",
             VOTE_BUTTON_TEXT = "Ovoz beirsh",
             CONTACT_TEXT = "Biz bilan a'loqa",
-            VOTE_MESSAGE = "Ovoz berish jarayoni muvaffaqiyatli yakunlangach, hisobingizga avtomatik tarzda 20,000 so'm pul mablag'i o'tkaziladi!\n" +
+            VOTE_MESSAGE = "Ovoz berish jarayoni muvaffaqiyatli yakunlangach, hisobingizga avtomatik tarzda 25,000 so'm pul mablag'i o'tkaziladi!\n" +
                     "\n" +
                     "Diqqat! Ovoz bermasdan turib \"✅ Ovoz berdim\" tugmasini bossangiz, botdan bloklanasiz!",
             VOTE_BUTTON_TG = "\uD83D\uDDE3 Ovoz berish (Telegram bot orqali)",
@@ -15,10 +15,10 @@ public class KeyWords {
             VOTE_BUTTON_WEB_URL = "https://openbudget.uz/boards/initiatives/initiative/32/c1b7b654-0bad-4c46-a8e3-2c0276f56a67",
             VOTE_BUTTON_WEB = "\uD83D\uDDE3 Ovoz berish (Sayt orqali)",
             VOTE_BUTTON_CONFIRM = "✅ Ovoz berdim",
-            VOTE_CHEK_MESSAGE = "24 soat ichida siz bergan ovoz qabul qilinganligi tekshiriliadi, agar siz bergan ovoz qabul qilingan bo'lsa pul avtomatik ravishda hisobingizga surib beriladi. Telgram botimizni boshqa tanishlaringizga ham ulashing :) ",
+            VOTE_CHEK_MESSAGE = "24 soat ichida siz bergan ovoz qabul qilinganligi tekshiriliadi, agar siz bergan ovoz qabul qilingan bo'lsa pul avtomatik ravishda hisobingizga surib beriladi. Telegram botimizni boshqa tanishlaringizga ham ulashing :) ",
             CONFIRM_BUTTON_ADMIN = "✅✅✅Ovoz qabul qilinganlini tekshirdim va tasdiqlayman ✅✅✅✅",
             NOT_CONFIRM_BUTTON_ADMIN = "❌❌❌Ovoz qabul qilinmadi❌❌❌❌",
             BLOC_USER = "Foydalanuvchini bloklash ❗\uFE0F ",
-            CONNECT_MESSAGE = "@Muhammadqodir_Abdullajonov ga a'loqaga chiqishligingiz mumkin";
+            CONNECT_MESSAGE = "@sherzodbek_gayipov ga a'loqaga chiqishligingiz mumkin";
 
 }
